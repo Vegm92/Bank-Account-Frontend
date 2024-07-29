b0474ff34a6646d4a6407f275e10113735e758c4
Victor Granda <victorgranda1992@gmail.com>
2024-07-29 12:50:54 +0200

    Add Navigation component from Mui and Home page to showcase the account Info

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,28 +1,120 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Button } from "@mui/material";
+import React, { useState } from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+const drawerWidth = 240;
+const navItems = [
+  { label: "Home", to: "/" },
+  { label: "Deposit", to: "/deposit" },
+  { label: "Withdraw", to: "/withdraw" },
+  { label: "Transfer", to: "/transfer" },
+  { label: "Statement", to: "/statement" },
+];
 
 const Navigation: React.FC = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const location = useLocation();
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Bank Account App
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.label} disablePadding>
+            <ListItemButton
+              component={RouterLink}
+              to={item.to}
+              selected={location.pathname === item.to}
+              sx={{ textAlign: "center" }}
+            >
+              <ListItemText primary={item.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/deposit">
-          Deposit
-        </Button>
-        <Button color="inherit" component={Link} to="/withdraw">
-          Withdraw
-        </Button>
-        <Button color="inherit" component={Link} to="/transfer">
-          Transfer
-        </Button>
-        <Button color="inherit" component={Link} to="/statement">
-          Statement
-        </Button>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Bank Account App
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item.label}
+                component={RouterLink}
+                to={item.to}
+                sx={{ color: "#fff" }}
+              >
+                {item.label}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
   );
 };
 
