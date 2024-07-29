deb579c1ad8a1dce1c2f912a0b0c2088735a0995
Victor Granda <victorgranda1992@gmail.com>
2024-07-29 15:50:08 +0200

    fix issues wwith variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getOrCreateIBAN } from "../utils/ibanUtils";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_URL = process.env.VITE_API_URL || "http://localhost:3000/api";
 
 type Transaction = {
   type: string;
