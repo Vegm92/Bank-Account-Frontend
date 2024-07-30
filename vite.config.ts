de1b2f9ad6e7dda4bfd6bb4769c98065a8971a39
Victor Granda <victorgranda1992@gmail.com>
2024-07-30 13:25:26 +0200

    Add setup & configuration files for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { UserConfig as VitestUserConfig } from "vitest/config";
+
+interface UserConfig extends VitestUserConfig {
+  test: {
+    globals: boolean;
+    environment: string;
+    setupFiles: string | string[];
+  };
+}
 
 export default defineConfig({
   plugins: [react()],
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+} as UserConfig);
